3820b576fa4250877a03d9a1b70240d006819a74
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-12 18:59:27 -0400

    Created an item table with 2 items for testing

--- backend/src/main/resources/initdb/starterData.sql
+++ backend/src/main/resources/initdb/starterData.sql
@@ -1,6 +1,11 @@
-CREATE TABLE test (
+CREATE TABLE store_item (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  title VARCHAR(50) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  -- Just to be safe
+  image_link VARCHAR(2000) NOT NULL
 );
 
-INSERT INTO test (name) VALUES ('Nice');
+INSERT INTO store_item (title, description, image_link) 
+  VALUES ('Classic Shirt', 'Our most popular shirt made of 100% finest quality cotton', 'https://img.sonofatailor.com/images/customizer/product/White_O_Crew_Regular_NoPocket.jpg'),
+         ('Basic Shirt', 'Our most basic shirt made of polyester', 'https://static.owayo-cdn.com/newhp/img/productHome/productSeitenansicht/productservice/tshirts_classic_herren_basic_productservice/st2020_whi.png');

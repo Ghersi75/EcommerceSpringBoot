ec9995e453e556b284efbcb4604f154f80b03cca
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-12 19:00:08 -0400

    Created ItemRepository to interact with db

---
+++ backend/src/main/java/com/Ecommerce/Learning/Repository/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.Ecommerce.Learning.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Ecommerce.Learning.Models.Item;;
+
+public interface ItemRepository extends JpaRepository<Item, Long> {
+  
+}

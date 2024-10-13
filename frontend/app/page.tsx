9009040a40266106ae56120ccfdaff16f0eb11e3
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-13 11:31:06 -0400

    Created basic fetching logic for testing getAllItems api endpoint

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 
+import ItemCard from "@/components/ItemCard";
 import Image from "next/image";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [data, setData] = useState(null)
+
+  useEffect(() => { 
+    async function FetchItems() {
+      let res = await fetch("localhost:8080/items")
+      let data = await res.json()
+      setData(data)
+    }
+
+    FetchItems()
+  }, [])
+
+  console.log(data)
+
   return (
-    <div>
-      Hola
+    <div className="p-4 w-svw h-svh flex items-center justify-center">
+      <ItemCard />
     </div>
   );
 }

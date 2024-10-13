bf407a23c71e831983859b8f001d74043033a0b8
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-13 11:46:56 -0400

    Made changes to test data fetching, works

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,24 +4,35 @@ import ItemCard from "@/components/ItemCard";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 
+interface Data {
+  id: String,
+  title: String,
+  description: String,
+  imageLink: String
+}
+
 export default function Home() {
-  const [data, setData] = useState(null)
+  const [data, setData] = useState<Data[]>([])
 
   useEffect(() => { 
     async function FetchItems() {
-      let res = await fetch("localhost:8080/items")
+      let res = await fetch("http://localhost:8080/items")
       let data = await res.json()
+      console.log(data)
       setData(data)
     }
 
     FetchItems()
   }, [])
 
-  console.log(data)
 
   return (
     <div className="p-4 w-svw h-svh flex items-center justify-center">
-      <ItemCard />
+      {data.map((item) => {
+        return (
+          <div> {item.id} </div>
+        )
+      })}
     </div>
   );
 }

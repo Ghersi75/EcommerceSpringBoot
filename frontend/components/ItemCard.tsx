46b930283897aabb889409cd4086cce70fef9942
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-14 17:21:38 -0400

    Updated carousel to take images from api, use placeholder if no images are returned, and to only display left/right arrow if at least 2 images are found

--- frontend/components/ItemCard.tsx
+++ frontend/components/ItemCard.tsx
@@ -3,31 +3,41 @@ import { Data } from "@/app/page";
 import { useState } from "react";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./shadcn/carousel";
 
-export default function ItemCard({ data }: { data: Data }) {
-  const [images, setImages] = useState([
-    "https://i.pinimg.com/236x/b2/09/c0/b209c05e517088a17d16f5401b6a616f.jpg",
-    "https://prd.cc.duluthtrading.com/on/demandware.static/-/Sites-dtc-master-catalog/default/dw13c50eee/images/large/76020_EBR.jpg",
-    "https://img.freepik.com/free-psd/stylish-blue-plaid-shirt-men-isolated-transparent-background_191095-23042.jpg"
-  ]);
+const placeholderImageLink = "https://cdn4.vectorstock.com/i/1000x1000/93/98/default-placeholder-fitness-trainer-in-a-t-shirt-vector-21189398.jpg"
 
+export default function ItemCard({ data }: { data: Data }) {
   return (
     <Card className="w-[400px]">
       <CardImageContainer>
         <Carousel className="relative">
           <CarouselContent>
-            {images.map((item, idx) => {
+            {
+              data.images.length == 0 &&
+              <CarouselItem>
+                <CardImage
+                  src={placeholderImageLink}
+                  className={`w-[400px] h-[200px] object-contain`}
+                />
+              </CarouselItem>
+            }
+            {data.images.map((item, idx) => {
               return (
                 <CarouselItem key={idx}>
                   <CardImage
-                    src={item}
-                    className={`w-[400px] h-[200px] object-contain`} 
+                    src={item.imageLink}
+                    className={`w-[400px] h-[200px] object-contain`}
                   />
                 </CarouselItem>
               )
             })}
           </CarouselContent>
-          <CarouselPrevious className="absolute left-4"/>
-          <CarouselNext className="absolute right-4" />
+          {
+            data.images.length > 1 &&
+            <>
+              <CarouselPrevious className="absolute left-4"/>
+              <CarouselNext className="absolute right-4" />
+            </>
+          }
         </Carousel>
       </CardImageContainer>
       <CardHeader>

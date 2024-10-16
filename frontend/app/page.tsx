2c40cfbdb9b8be0a77bb24e850946be9e309bd42
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-16 18:21:32 -0400

    Cleaned up code and added future TODOs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,32 +1,28 @@
 "use client";
 
 import ItemCard from "@/components/ItemCard";
-import { ItemData } from "@/lib/types/StoreItemTypes";
-import { compareDisplayOrder } from "@/lib/utils";
-import { useState, useEffect } from "react";
+import useFetchItems from "@/lib/hooks/useFetchItems";
 
 export default function Home() {
-  const [data, setData] = useState<ItemData[]>([])
+  const { data, isLoading, error } = useFetchItems();
 
-  useEffect(() => { 
-    async function FetchItems() {
-      let res = await fetch("http://localhost:8080/items")
-      let resultData = await res.json()
-      resultData.sort(compareDisplayOrder)
-      setData(resultData)
-    }
-
-    FetchItems()
-  }, [])
-
-
+  if (error) {
+    // TODO add actual error handling logic
+    console.log(error)
+  }
+  
   return (
-    <div className="p-4 w-svw flex items-center justify-start gap-4">
-      {data.map((item, id) => {
-        return (
-          <ItemCard data={item} key={id} />
-        )
-      })}
-    </div>
+    <>
+    {/* TODO Create a scheleton component for loading */}
+      {isLoading  ? <div> loading </div> :
+        <div className="p-4 w-svw flex items-center justify-start gap-4">
+          {data.map((item, id) => {
+            return (
+              <ItemCard data={item} key={id} />
+            )
+          })}
+        </div>
+      }
+    </>
   );
 }

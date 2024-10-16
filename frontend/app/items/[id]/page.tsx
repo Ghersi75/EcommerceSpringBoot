defc88cdd65b3edb274c91ae3537069b8c17cba4
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-16 18:34:05 -0400

    Used hook, added conditions for rendering and a couple TODOs

--- frontend/app/items/[id]/page.tsx
+++ frontend/app/items/[id]/page.tsx
@@ -1,13 +1,30 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import useFetchItemById from "@/lib/hooks/useFetchItemById"
 
 export default function ViewItem({ params }: { params: { id: number } }) {
-  
+  const { data, isLoading, error } = useFetchItemById(params.id)
 
+  if (error) {
+    // TODO add actual error handling logic
+    console.log(error)
+  }
+
+  if (isLoading) {
+    return (<div> loading </div>)
+  }
+
+  // Not laoding and no data, something went wrong
+  // TODO: Figure out what to do when no data loads for whatever reason. Probably linked to the if (error)
+  if (!data) {
+    return (<div> whoopsie daisy</div>)
+  }
+
+  // No need to worry about data being empty and having a bunch of ????????????????? here
   return (
     <div>
-      {params.id}
+      {data.title}
+      {data.description}
     </div>
   )
 }

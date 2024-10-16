3dd248c585efdb9e4950bcc287ed784a4552391b
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-16 18:02:02 -0400

    Created compareDisplayOrder function to sort any item that has a displayOrder field

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// Interface used for sorting function below
+interface DisplayOrderInterface { 
+  displayOrder: number
+}
+
+// Function used to sort any item that has a "displayOrder" field
+export function compareDisplayOrder<T extends DisplayOrderInterface> (item1: T, item2: T) {
+  if ( item1.displayOrder < item2.displayOrder ) {
+    return -1;
+  } else {
+    return 1
+  }
+}

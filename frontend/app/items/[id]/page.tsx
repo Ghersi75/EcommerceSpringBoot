5e68e6b6b8870ee121c89ba88f72b6793766f829
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-10-17 19:40:00 -0400

    Switched to much cleaner masonry layout with columns. Will need to change it later and reference actual images, but masonry layout is the plan

--- frontend/app/items/[id]/page.tsx
+++ frontend/app/items/[id]/page.tsx
@@ -5,8 +5,13 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 import useFetchItemById from "@/lib/hooks/useFetchItemById"
 
 const testImages = [
+  "https://img.freepik.com/premium-photo/man-wearing-shirt-that-says-he-is-wearing-shirt_1313274-13659.jpg?w=1380",
+  "https://img.freepik.com/free-photo/shirt_1203-8194.jpg?w=1800&t=st=1729207891~exp=1729208491~hmac=a591c163f01c9e0e45c88112ea1f33741f5eb040c01bd30ce8a0b871ca15e553",
+  "https://img.freepik.com/premium-photo/blue-plaid-shirt-with-white-collar-blue-jeans_1086760-50674.jpg?w=2000",
   "https://img.freepik.com/free-psd/stylish-blue-plaid-shirt-men-isolated-transparent-background_191095-23042.jpg?w=1380&t=st=1729206787~exp=1729207387~hmac=46e94f52ac1b8c57aceaa42fd1ff16798515d69aa26c44d83ce8c78d5703c645",
-  "https://media.gettyimages.com/id/471188329/photo/plain-red-tee-shirt-isolated-on-white-background.jpg?s=612x612&w=gi&k=20&c=RGOHoV-pQqfvG3gMgKpa4PNbV5gUJPY3SniV1ao9yGM="
+  "https://media.gettyimages.com/id/471188329/photo/plain-red-tee-shirt-isolated-on-white-background.jpg?s=612x612&w=gi&k=20&c=RGOHoV-pQqfvG3gMgKpa4PNbV5gUJPY3SniV1ao9yGM=",
+  "https://img.freepik.com/premium-photo/man-wearing-plaid-shirt-stands-front-window_1313274-13213.jpg?w=1380",
+  "https://img.freepik.com/premium-photo/man-wear-white-short-sleeve-button-down-mockup_1290684-1661.jpg?w=1380"
 ]
 
 export default function ViewItem({ params }: { params: { id: number } }) {
@@ -31,22 +36,14 @@ export default function ViewItem({ params }: { params: { id: number } }) {
   // Figure out design later, im no designer
   return (
     <div className="flex flex-1 flex-row h-full w-3/4">
-      <div className="bg-blue-500 w-2/3 p-12 flex justify-center">
-        <div className="w-2/3 h-full bg-red-500">
-          <Carousel className="relative">
-            <CarouselContent>
-            {testImages.map((item, idx) => {
-              return (
-                <CarouselItem>
-                  <CardImage src={item} key={idx} className="rounded-xl"/>
-                </CarouselItem>
-              )
-            })}
-            </CarouselContent>
-            <CarouselNext className="absolute right-12" />
-            <CarouselPrevious className="absolute left-12" />
-          </Carousel>
-        </div>
+      <div className="container bg-blue-500 w-2/3 p-8 columns-3">
+        {testImages.map((item, idx) => {
+          return (
+            <div className="h-fit p-4">
+              <img src={item} />
+            </div>
+          )
+        })}
       </div>
       <div className="bg-green-500 w-1/3 p-4"> Modifiers and other stuff </div>
     </div>

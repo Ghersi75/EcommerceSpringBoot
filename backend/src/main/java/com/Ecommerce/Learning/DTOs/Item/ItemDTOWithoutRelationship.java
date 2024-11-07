b50b3854474d1d09d9cad4c9769c69ba4c457dde
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-11-07 13:52:24 -0500

    Added constructor that takes Item object directly. Used for JPA Repository custom query

--- backend/src/main/java/com/Ecommerce/Learning/DTOs/Item/ItemDTOWithoutRelationship.java
+++ backend/src/main/java/com/Ecommerce/Learning/DTOs/Item/ItemDTOWithoutRelationship.java
@@ -1,5 +1,7 @@
 package com.Ecommerce.Learning.DTOs.Item;
 
+import com.Ecommerce.Learning.Models.Item;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -13,4 +15,15 @@ public class ItemDTOWithoutRelationship {
   private int price;
   private Integer discountPrice;
   private String thumbnailImageLink;
+
+  // Used for repository query that takes Item directly as parameter
+  public ItemDTOWithoutRelationship(Item item) {
+    this.id = item.getId();
+    this.title = item.getTitle();
+    this.description = item.getDescription();
+    this.displayOrder = item.getDisplayOrder();
+    this.price = item.getPrice();
+    this.discountPrice = item.getDiscountPrice();
+    this.thumbnailImageLink = item.getThumbnailImageLink();
+  }
 }

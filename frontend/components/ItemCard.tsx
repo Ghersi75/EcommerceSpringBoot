ef98935131fc82c1cd496afb79fc3ca18dc73e90
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-11-20 11:44:19 -0500

    Updated ItemCard to reflect type changes and added discountPrice logic

--- frontend/components/ItemCard.tsx
+++ frontend/components/ItemCard.tsx
@@ -1,34 +1,39 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardImage, CardImageContainer, CardTitle } from "./shadcn/card";
-import { ItemData } from "@/lib/types/StoreItemTypes";
+import { StoreItemData } from "@/lib/types/StoreItemTypes";
 import { useState } from "react";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "./shadcn/carousel";
 import { Button } from "./shadcn/button";
 import Link from "next/link";
+import { formatPrice } from "@/lib/utils";
 
-// TODO: Fix this to work with the new endpoint response
-
-const placeholderImageLink = "https://cdn4.vectorstock.com/i/1000x1000/93/98/default-placeholder-fitness-trainer-in-a-t-shirt-vector-21189398.jpg"
-
-export default function ItemCard({ data }: { data: ItemData }) {
+export default function ItemCard({ data }: { data: StoreItemData }) {
   return (
     <Card className="w-[400px]">
       <CardImageContainer>
-        <Carousel className="relative">
-          <CarouselContent>
-            <CarouselItem>
-              <CardImage
-                src={placeholderImageLink}
-                className={`w-[400px] h-[200px] object-contain`}
-              />
-            </CarouselItem>
-          </CarouselContent>
-        </Carousel>
+        <CardImage
+          src={data.thumbnailImageLink}
+          className={`w-[400px] h-[200px] object-contain`}
+        />
       </CardImageContainer>
       <CardHeader>
         <CardTitle>{data.title}</CardTitle>
         <CardDescription>{data.description}</CardDescription>
       </CardHeader>
-      <CardFooter className="flex justify-end">
+      <CardFooter className="flex justify-between">
+        <CardContent className="p-4">
+          {
+            // TODO: Make backend return formatted price
+            data.discountPrice ?
+            <p className="flex gap-2 items-end">
+              {formatPrice(data.discountPrice)}
+              <p className="line-through text-gray-500 text-sm">
+                {formatPrice(data.price)}
+              </p>
+            </p> 
+            :
+            <p>{formatPrice(data.price)}</p>
+          }
+        </CardContent>
         <Link href={`/items/${data.id}`}>
           <Button> View More </Button>
         </Link>
